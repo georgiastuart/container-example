ee94af1891b9891356262868cb59965a98c36fb0
Georgia Stuart <georgia.stuart@gmail.com>
2021-03-09 17:58:59 -0600

    Add singularity def files

--- test/test.py
+++ test/test.py
@@ -4,26 +4,26 @@ import h5py
 
 class TestMatrixSerial(unittest.TestCase):
     def test_serial_rectangle(self):
-        run(['../build/random_matrix', '10', '40'])
+        run(['random_matrix', '10', '40'])
         with h5py.File('matrix.hdf5', 'r') as fp:
             self.assertEqual(10, fp['matrix'][:, :].shape[0])
             self.assertEqual(40, fp['matrix'][:, :].shape[1])
 
     def test_square(self):
-        run(['../build/random_matrix', '40'])
+        run(['random_matrix', '40'])
         with h5py.File('matrix.hdf5', 'r') as fp:
             self.assertEqual(40, fp['matrix'][:, :].shape[0])
             self.assertEqual(40, fp['matrix'][:, :].shape[1])
 
 class TestMatrixParallel(unittest.TestCase):
     def test_rectangle(self):
-        run(['mpirun', '-np', '4', '../build/random_matrix', '10', '40'])
+        run(['mpirun', '-np', '4', 'random_matrix', '10', '40'])
         with h5py.File('matrix.hdf5', 'r') as fp:
             self.assertEqual(10, fp['matrix'][:, :].shape[0])
             self.assertEqual(40, fp['matrix'][:, :].shape[1])
 
     def test_square(self):
-        run(['mpirun', '-np', '4', '../build/random_matrix', '40', '40'])
+        run(['mpirun', '-np', '4', 'random_matrix', '40', '40'])
         with h5py.File('matrix.hdf5', 'r') as fp:
             self.assertEqual(40, fp['matrix'][:, :].shape[0])
             self.assertEqual(40, fp['matrix'][:, :].shape[1])
